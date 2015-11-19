9311a383528dee37362b953156b6009c1ad49bcf
Hedgeriot <john.mace@ucc.on.ca>
2015-11-19 17:47:12 -0500

    More changes to Aegisalt.java
    Added a recipe. Currently doesn’t work.

--- java/aegisalt/Aegisalt.java
+++ java/aegisalt/Aegisalt.java
@@ -1,6 +1,16 @@
 package aegisalt;
 
-
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemArmor.ArmorMaterial;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
@@ -28,6 +38,14 @@ public class Aegisalt {
    	   	 // Register your custom block or item with Minecraft
             GameRegistry.registerBlock(AegisaltOreBlock, modid + AegisaltOreBlock.getUnlocalizedName().substring(5));
             GameRegistry.registerItem(AegisaltDust, modid + AegisaltDust.getUnlocalizedName().substring(5));
+            
+            GameRegistry.addShapedRecipe(new ItemStack(AegisaltDust), //This meantion of AegisaltDust is what the recipe will need
+            
+            "AAA",
+            "AAA",
+            "AAA",
+            
+            'A', new ItemStack(Aegisalt.AegisaltDust)); //This is the required input for the recipe
   
       }
 	
